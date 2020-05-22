7aaec7e403239438a534b83b06b74f19caedfdf1
devdu <thalison.eduardo14@gmail.com>
2020-05-22 01:09:38 -0300

    add directions controls and limited space box

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let direction = 'right';
 
 let game = setInterval(initGame, 100);
 
+document.addEventListener('keydown', update);
+
 function createBg() {
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box)
@@ -25,6 +27,12 @@ function createSnake() {
 }
 
 function initGame() {
+
+    if (snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+    if (snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
+    if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
+
     createBg();
     createSnake();
 
@@ -33,8 +41,8 @@ function initGame() {
 
     if (direction === 'right') snakeX += box;
     if (direction === 'left') snakeX -= box;
-    if (direction === 'up') snakeY += box;
-    if (direction === 'down') snakeY -= box;
+    if (direction === 'up') snakeY -= box;
+    if (direction === 'down') snakeY += box;
 
     snake.pop();
 
@@ -46,4 +54,11 @@ function initGame() {
     snake.unshift(newHead);
 }
 
+function update(event) {
+    let key = event.key;
 
+    if (key === "ArrowLeft" && direction !== 'right') direction = 'left';
+    if (key === "ArrowUp" && direction !== 'down') direction = 'up';
+    if (key === "ArrowRight" && direction !== 'left') direction = 'right';
+    if (key === "ArrowDown" && direction !== 'up') direction = 'down';
+}
